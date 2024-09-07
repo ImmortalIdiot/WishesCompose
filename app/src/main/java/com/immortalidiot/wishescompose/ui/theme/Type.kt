75e90bd25a17501e29a5a32fc479d684c470ac7f
ImmortalIdiot <max.krasulya@yandex.ru>
2024-09-08 02:08:44 +0300

    feat: add the necessary text styles

--- app/src/main/java/com/immortalidiot/wishescompose/ui/theme/Type.kt
+++ app/src/main/java/com/immortalidiot/wishescompose/ui/theme/Type.kt
@@ -2,9 +2,11 @@ package com.immortalidiot.wishescompose.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.immortalidiot.wishescompose.R
 
 // Set of Material typography styles to start with
 val Typography = Typography(
@@ -15,20 +17,39 @@ val Typography = Typography(
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
     )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
-    ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
-    )
-    */
 )
+
+val ArchivoFontFamily = FontFamily(
+    Font(R.font.archivo_light, FontWeight.Light),
+    Font(R.font.archivo_medium, FontWeight.Medium),
+    Font(R.font.archivo_semibold, FontWeight.SemiBold)
+)
+
+val TomorrowFontFamily = FontFamily(
+    Font(R.font.tomorrow_light, FontWeight.Light)
+)
+
+private val lightArchivoStyle = TextStyle(
+    fontFamily = ArchivoFontFamily,
+    fontWeight = FontWeight.Light
+)
+
+private val mediumArchivoStyle = TextStyle(
+    fontFamily = ArchivoFontFamily,
+    fontWeight = FontWeight.Medium
+)
+
+private val semiBoldArchivoStyle = TextStyle(
+    fontFamily = ArchivoFontFamily,
+    fontWeight = FontWeight.SemiBold
+)
+
+private val lightTomorrowStyle = TextStyle(
+    fontFamily = TomorrowFontFamily,
+    fontWeight = FontWeight.Light
+)
+
+val modeHeaderText = mediumArchivoStyle.copy(fontSize = 30.sp)
+val buttonText = semiBoldArchivoStyle.copy(fontSize = 24.sp)
+val headerText = lightArchivoStyle.copy(fontSize = 22.sp)
+val hintText = lightTomorrowStyle.copy(fontSize = 22.sp)
