0f637dca3e6ea3439455a3aa11beee2287c3c132
ImmortalIdiot <max.krasulya@yandex.ru>
2024-09-08 01:38:17 +0300

    feat: add the necessary colors

--- app/src/main/java/com/immortalidiot/wishescompose/ui/theme/Color.kt
+++ app/src/main/java/com/immortalidiot/wishescompose/ui/theme/Color.kt
@@ -8,4 +8,14 @@ val Pink80 = Color(0xFFEFB8C8)
 
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val Pink40 = Color(0xFF0A1330)
+
+val ButtonStart = Color(0xFF8C9CEB)
+val ButtonEnd = Color(0xFF8B48CC)
+
+val ButtonBackground = Color(0xFF8A34DD)
+
+val BackgroundEnd = Color(0xFF234CB8)
+val BackgroundStart = Color(0xFF0A1330)
+
+val HintColor = Color(0xFF929292)
