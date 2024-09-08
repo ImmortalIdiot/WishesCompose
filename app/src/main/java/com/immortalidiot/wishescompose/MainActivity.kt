7cccfd3b58ad0857b5dbd1e1874953891ce7be5b
ImmortalIdiot <max.krasulya@yandex.ru>
2024-09-08 03:18:17 +0300

    chore: remove unused import

--- app/src/main/java/com/immortalidiot/wishescompose/MainActivity.kt
+++ app/src/main/java/com/immortalidiot/wishescompose/MainActivity.kt
@@ -6,7 +6,6 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
