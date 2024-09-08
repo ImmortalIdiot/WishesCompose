0f3b0127a2afcaa6bf27787f2e7d636b3dc779df
ImmortalIdiot <max.krasulya@yandex.ru>
2024-09-08 16:45:16 +0300

    feat: add dimensions for the button

---
+++ app/src/main/java/com/immortalidiot/wishescompose/ui/theme/Dimensions.kt
@@ -0,0 +1,17 @@
+package com.immortalidiot.wishescompose.ui.theme
+
+import androidx.compose.runtime.compositionLocalOf
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+
+data class Dimensions(
+    val roundedCorners: Dp = 8.dp
+)
+
+data class DimensionsTest(
+    val maxHeightButtonTest: Dp = 54.dp,
+    val maxWidthButtonTest: Dp = 280.dp,
+)
+
+val LocalDimensions = compositionLocalOf { Dimensions() }
+val LocalTestDimensions = compositionLocalOf { DimensionsTest() }
