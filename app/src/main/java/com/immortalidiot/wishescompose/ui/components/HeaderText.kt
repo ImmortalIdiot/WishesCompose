897f7f93ec05dd8de62e3b5dcdc39fc66423c88d
ImmortalIdiot <max.krasulya@yandex.ru>
2024-09-08 16:55:33 +0300

    refactor: make header text component more flexible

--- app/src/main/java/com/immortalidiot/wishescompose/ui/components/HeaderText.kt
+++ app/src/main/java/com/immortalidiot/wishescompose/ui/components/HeaderText.kt
@@ -4,18 +4,20 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import com.immortalidiot.wishescompose.R
 import com.immortalidiot.wishescompose.ui.theme.modeHeaderText
 
 @Composable
-fun ModeHeaderText(
-    text: String
+fun HeaderText(
+    text: String,
+    textStyle: TextStyle
 ) {
     Text(
         text = text,
-        style = modeHeaderText,
+        style = textStyle,
         color = Color.White,
         textAlign = TextAlign.Center
     )
@@ -24,5 +26,8 @@ fun ModeHeaderText(
 @Preview
 @Composable
 fun ModeHeaderTextPreview() {
-    ModeHeaderText(text = stringResource(id = R.string.selection_mode))
+    HeaderText(
+        text = stringResource(id = R.string.selection_mode),
+        textStyle = modeHeaderText
+    )
 }
