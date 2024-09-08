c765f6e9819d298e4cd77bf900104dc5722be033
ImmortalIdiot <max.krasulya@yandex.ru>
2024-09-08 03:17:42 +0300

    feat: set the background color for the whole activity

--- app/src/main/java/com/immortalidiot/wishescompose/MainActivity.kt
+++ app/src/main/java/com/immortalidiot/wishescompose/MainActivity.kt
@@ -3,37 +3,49 @@ package com.immortalidiot.wishescompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import com.immortalidiot.wishescompose.ui.theme.BackgroundEnd
+import com.immortalidiot.wishescompose.ui.theme.BackgroundStart
 import com.immortalidiot.wishescompose.ui.theme.WishesComposeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            WishesComposeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+            Box(
+                modifier = Modifier
+                    .background(
+                        brush = Brush.linearGradient(
+                            colors = listOf(
+                                BackgroundStart,
+                                BackgroundEnd
+                            )
+                        )
+                    )
+                    .fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ) {
+                Greeting("Android")
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
+fun Greeting(name: String) {
     Text(
         text = "Hello $name!",
-        modifier = modifier
+        color = Color.White
     )
 }
 
