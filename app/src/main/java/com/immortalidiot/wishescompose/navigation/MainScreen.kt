1c2f26f9be2bfbc1a239e717ec8748b859b80856
ImmortalIdiot <max.krasulya@yandex.ru>
2024-09-14 01:41:05 +0300

    feat: add the main screen with all screens

---
+++ app/src/main/java/com/immortalidiot/wishescompose/navigation/MainScreen.kt
@@ -0,0 +1,11 @@
+package com.immortalidiot.wishescompose.navigation
+
+import androidx.compose.runtime.Immutable
+
+@Immutable
+sealed class MainScreen(val route: String) {
+    data object EmojiGeneratorScreen: MainScreen("EmojiGenerator")
+    data object DayWishGeneratorScreen: MainScreen("DayWishGenerator")
+    data object NightWishGeneratorScreen: MainScreen("NightWishGenerator")
+    data object DevelopersScreen: MainScreen("Developers")
+}
