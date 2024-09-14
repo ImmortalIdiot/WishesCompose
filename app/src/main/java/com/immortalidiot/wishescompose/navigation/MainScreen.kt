cb485f2bc8714ffc41fdc461e31b740e1f1477cb
ImmortalIdiot <max.krasulya@yandex.ru>
2024-09-14 14:23:49 +0300

    feat: add the mode selection screen object

--- app/src/main/java/com/immortalidiot/wishescompose/navigation/MainScreen.kt
+++ app/src/main/java/com/immortalidiot/wishescompose/navigation/MainScreen.kt
@@ -4,6 +4,7 @@ import androidx.compose.runtime.Immutable
 
 @Immutable
 sealed class MainScreen(val route: String) {
+    data object ModeSelectionScreen: MainScreen("ModeSelection")
     data object EmojiGeneratorScreen: MainScreen("EmojiGenerator")
     data object DayWishGeneratorScreen: MainScreen("DayWishGenerator")
     data object NightWishGeneratorScreen: MainScreen("NightWishGenerator")
