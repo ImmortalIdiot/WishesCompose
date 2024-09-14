b7bc61ddee963cfd57b3d9aab8e191cdab1129dc
ImmortalIdiot <max.krasulya@yandex.ru>
2024-09-14 16:23:24 +0300

    feat: add the testing develop screen

--- app/src/main/java/com/immortalidiot/wishescompose/screens/DevelopersScreen.kt
+++ app/src/main/java/com/immortalidiot/wishescompose/screens/DevelopersScreen.kt
@@ -1,2 +1,37 @@
 package com.immortalidiot.wishescompose.screens
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.navigation.NavHostController
+import com.immortalidiot.wishescompose.R
+import com.immortalidiot.wishescompose.ui.components.HeaderText
+import com.immortalidiot.wishescompose.ui.theme.LocalDimensions
+import com.immortalidiot.wishescompose.ui.theme.headerText
+
+@Composable
+fun DevelopersScreen(
+    navHostController: NavHostController,
+    modifier: Modifier = Modifier
+) {
+    val dimensions = LocalDimensions.current
+
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .padding(vertical = dimensions.verticalSmallPadding),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        HeaderText(
+            text = stringResource(R.string.developer),
+            textStyle = headerText.copy(fontWeight = FontWeight.Bold)
+        )
+    }
+}
