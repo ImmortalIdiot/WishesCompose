f0404b022504723c0607f51eb9277de7a39274b1
ImmortalIdiot <max.krasulya@yandex.ru>
2024-09-14 16:13:37 +0300

    refactor: disable the ripple effect and change the background color when is pressed

--- app/src/main/java/com/immortalidiot/wishescompose/ui/components/PrimaryButton.kt
+++ app/src/main/java/com/immortalidiot/wishescompose/ui/components/PrimaryButton.kt
@@ -1,14 +1,17 @@
 package com.immortalidiot.wishescompose.ui.components
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.interaction.collectIsPressedAsState
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -18,6 +21,7 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import com.immortalidiot.wishescompose.R
+import com.immortalidiot.wishescompose.ui.theme.ButtonBackground
 import com.immortalidiot.wishescompose.ui.theme.ButtonEnd
 import com.immortalidiot.wishescompose.ui.theme.ButtonStart
 import com.immortalidiot.wishescompose.ui.theme.LocalDimensions
@@ -34,23 +38,31 @@ fun PrimaryButton(
 ) {
     val dimensions = LocalDimensions.current
     val roundedCornerShape = RoundedCornerShape(dimensions.roundedCorners)
-    val colors = ButtonDefaults.buttonColors(Color.Transparent)
 
-    Button(
+    val interactionSource = remember { MutableInteractionSource() }
+    val isPressed by interactionSource.collectIsPressedAsState()
+    val backgroundColors = if (isPressed) {
+        listOf(ButtonBackground, ButtonBackground)
+    } else {
+        listOf(ButtonStart, ButtonEnd)
+    }
+
+    Box(
         modifier = modifier
             .height(maxHeight)
             .width(maxWidth)
             .clip(shape = roundedCornerShape)
             .background(
                 brush = Brush.verticalGradient(
-                    colors = listOf(
-                        ButtonStart,
-                        ButtonEnd
-                    )
+                    colors = backgroundColors
                 )
+            )
+            .clickable(
+                interactionSource = interactionSource,
+                indication = null,
+                onClick = onClick
             ),
-        colors = colors,
-        onClick = onClick
+        contentAlignment = Alignment.Center
     ) {
         Box(contentAlignment = Alignment.Center) {
             Text(
