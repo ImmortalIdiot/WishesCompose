ba91b5a490292e4647d43cdf08e61ae4a33618a8
ImmortalIdiot <max.krasulya@yandex.ru>
2024-09-14 16:13:01 +0300

    feat: add the mode selection screen

--- app/src/main/java/com/immortalidiot/wishescompose/screens/ModeSelectionScreen.kt
+++ app/src/main/java/com/immortalidiot/wishescompose/screens/ModeSelectionScreen.kt
@@ -1,2 +1,85 @@
 package com.immortalidiot.wishescompose.screens
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.navigation.NavHostController
+import com.immortalidiot.wishescompose.R
+import com.immortalidiot.wishescompose.ui.components.PrimaryButton
+import com.immortalidiot.wishescompose.ui.theme.LocalDimensions
+import com.immortalidiot.wishescompose.ui.theme.modeHeaderText
+
+@Composable
+fun ModeSelectionScreen(
+    navHostController: NavHostController,
+    modifier: Modifier = Modifier,
+) {
+    val dimension = LocalDimensions.current
+
+    Column(
+        modifier = modifier.fillMaxSize().padding(vertical = dimension.verticalBigPadding),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.SpaceBetween
+    ) {
+        Text(
+            text = stringResource(R.string.selection_mode),
+            fontSize = modeHeaderText.fontSize,
+            color = Color.White,
+            fontStyle = modeHeaderText.fontStyle,
+            fontFamily = modeHeaderText.fontFamily,
+            fontWeight = FontWeight.Bold
+        )
+
+        Column(
+            verticalArrangement = Arrangement.spacedBy(dimension.verticalSmallPadding)
+        ) {
+            PrimaryButton(
+                modifier = modifier,
+                maxWidth = dimension.bigWidthButton,
+                maxHeight = dimension.heightButton,
+                text = stringResource(R.string.emoji_generator),
+                onClick = {
+                    // TODO: move to emoji generator
+                }
+            )
+
+            PrimaryButton(
+                modifier = modifier,
+                maxWidth = dimension.bigWidthButton,
+                maxHeight = dimension.heightButton,
+                text = stringResource(R.string.day_wish_generator),
+                onClick = {
+                    // TODO: move to day wish generator
+                }
+            )
+
+            PrimaryButton(
+                modifier = modifier,
+                maxWidth = dimension.bigWidthButton,
+                maxHeight = dimension.heightButton,
+                text = stringResource(R.string.night_wish_generator_mode),
+                onClick = {
+                    // TODO: move to night wish generator
+                }
+            )
+        }
+
+        PrimaryButton(
+            modifier = modifier,
+            maxHeight = dimension.heightButton,
+            maxWidth = dimension.smallWidthButton,
+            text = stringResource(R.string.developers),
+            onClick = {
+                // TODO: move to developers
+            }
+        )
+    }
+}
