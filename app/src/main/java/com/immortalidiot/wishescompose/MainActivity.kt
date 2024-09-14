6c7bf5239a8b3f3055c4ae41842a4ba8a4b81a84
ImmortalIdiot <max.krasulya@yandex.ru>
2024-09-14 16:16:34 +0300

    refactor: remove greeting and preview functions

--- app/src/main/java/com/immortalidiot/wishescompose/MainActivity.kt
+++ app/src/main/java/com/immortalidiot/wishescompose/MainActivity.kt
@@ -8,18 +8,13 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.compose.rememberNavController
 import com.immortalidiot.wishescompose.navigation.main.MainScreenFlow
 import com.immortalidiot.wishescompose.ui.theme.BackgroundEnd
 import com.immortalidiot.wishescompose.ui.theme.BackgroundStart
-import com.immortalidiot.wishescompose.ui.theme.WishesComposeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -48,21 +43,5 @@ class MainActivity : ComponentActivity() {
                 MainScreenFlow(navHostController = navHostController)
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(
-        text = "Hello $name!",
-        color = Color.White
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    WishesComposeTheme {
-        Greeting("Android")
     }
 }
