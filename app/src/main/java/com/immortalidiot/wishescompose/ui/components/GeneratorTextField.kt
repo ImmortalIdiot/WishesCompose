d2739f453680c2af026900fa4c7f51c434c9b77c
ImmortalIdiot <max.krasulya@yandex.ru>
2024-09-21 18:34:00 +0300

    refactor: edit the generator input text field

--- app/src/main/java/com/immortalidiot/wishescompose/ui/components/GeneratorTextField.kt
+++ app/src/main/java/com/immortalidiot/wishescompose/ui/components/GeneratorTextField.kt
@@ -1,5 +1,8 @@
 package com.immortalidiot.wishescompose.ui.components
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.text.KeyboardOptions
@@ -7,41 +10,53 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import com.immortalidiot.wishescompose.R
+import com.immortalidiot.wishescompose.ui.theme.BackgroundEnd
+import com.immortalidiot.wishescompose.ui.theme.BackgroundStart
+import com.immortalidiot.wishescompose.ui.theme.HintColor
 import com.immortalidiot.wishescompose.ui.theme.LocalDimensions
 import com.immortalidiot.wishescompose.ui.theme.hintText
 
 @Composable
 fun GeneratorTextField(
-    value: String,
     onValueChange: (String) -> Unit,
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
+    value: String = ""
 ) {
     val dimensions = LocalDimensions.current
     val keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.NumberPassword)
+    val inputColor = HintColor
     val colors = TextFieldDefaults.colors(
         focusedContainerColor = Color.Transparent,
         unfocusedContainerColor = Color.Transparent,
         disabledContainerColor = Color.Transparent,
-        errorContainerColor = Color.Transparent
+        errorContainerColor = Color.Transparent,
+        focusedIndicatorColor = Color.White,
+        unfocusedIndicatorColor = Color.White,
+        cursorColor = Color.White
     )
-
     TextField(
         value = value,
         onValueChange = onValueChange,
         modifier = modifier.width(dimensions.maxWidthTextField),
-        textStyle = hintText,
+        textStyle = hintText.copy(
+            color = inputColor,
+            textAlign = TextAlign.Center
+        ),
         placeholder = {
             Text(
                 text = stringResource(id = R.string.min_and_max_hint),
                 modifier = Modifier.fillMaxWidth(),
-                textAlign = TextAlign.Center
+                textAlign = TextAlign.Center,
+                color = inputColor
             )
         },
         colors = colors,
@@ -52,8 +67,22 @@ fun GeneratorTextField(
 @Preview
 @Composable
 fun GeneratorTextFieldPreview() {
-    GeneratorTextField(
-        value = "",
-        onValueChange = { TODO() },
-    )
+    Box(
+        modifier = Modifier
+            .background(
+                brush = Brush.linearGradient(
+                    colors = listOf(
+                        BackgroundStart,
+                        BackgroundEnd
+                    )
+                )
+            )
+            .fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        GeneratorTextField(
+            value = "",
+            onValueChange = { TODO() },
+        )
+    }
 }
