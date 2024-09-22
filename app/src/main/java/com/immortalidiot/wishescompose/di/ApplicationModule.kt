119529e57fb3befe4b841c59222de259bd967584
ImmortalIdiot <max.krasulya@yandex.ru>
2024-09-22 18:37:50 +0300

    feat: add the di module

---
+++ app/src/main/java/com/immortalidiot/wishescompose/di/ApplicationModule.kt
@@ -0,0 +1,21 @@
+package com.immortalidiot.wishescompose.di
+
+import com.immortalidiot.wishescompose.logic.EmojiGenerator
+import com.immortalidiot.wishescompose.logic.WishGenerator
+import com.immortalidiot.wishescompose.logic.impl.EmojiGeneratorImpl
+import com.immortalidiot.wishescompose.logic.impl.WishGeneratorImpl
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+interface ApplicationModule {
+
+    @Binds
+    fun bindEmojiGenerator(impl: EmojiGeneratorImpl): EmojiGenerator
+
+    @Binds
+    fun bindWishGenerator(impl: WishGeneratorImpl): WishGenerator
+}
