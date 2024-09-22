3c14e6e692fc1fe237096da37679e2602dc8ea57
ImmortalIdiot <max.krasulya@yandex.ru>
2024-09-22 18:10:57 +0300

    feat: mark the main activity like the entry point

--- app/src/main/java/com/immortalidiot/wishescompose/MainActivity.kt
+++ app/src/main/java/com/immortalidiot/wishescompose/MainActivity.kt
@@ -15,7 +15,9 @@ import androidx.navigation.compose.rememberNavController
 import com.immortalidiot.wishescompose.navigation.main.MainScreenFlow
 import com.immortalidiot.wishescompose.ui.theme.BackgroundEnd
 import com.immortalidiot.wishescompose.ui.theme.BackgroundStart
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         enableEdgeToEdge(
