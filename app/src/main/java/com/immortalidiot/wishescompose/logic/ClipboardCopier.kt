8d8dcf25df4331671c6da94f6d16358c926090f0
ImmortalIdiot <max.krasulya@yandex.ru>
2024-09-25 13:47:49 +0300

    refactor: pass the context to the copy method

--- app/src/main/java/com/immortalidiot/wishescompose/logic/ClipboardCopier.kt
+++ app/src/main/java/com/immortalidiot/wishescompose/logic/ClipboardCopier.kt
@@ -1,5 +1,7 @@
 package com.immortalidiot.wishescompose.logic
 
+import android.content.Context
+
 interface ClipboardCopier {
-    suspend fun copy(copiedMessage: String)
+    suspend fun copy(context: Context, copiedMessage: String)
 }
