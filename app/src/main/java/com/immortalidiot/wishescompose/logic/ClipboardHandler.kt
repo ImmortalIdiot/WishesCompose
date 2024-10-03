dc76e243c29d8c1e67b49a1f787fdedce1fe6c80
ImmortalIdiot <max.krasulya@yandex.ru>
2024-10-03 20:05:55 +0300

    refactor: replace the null-safety type with a not-nullable type

--- app/src/main/java/com/immortalidiot/wishescompose/logic/ClipboardHandler.kt
+++ app/src/main/java/com/immortalidiot/wishescompose/logic/ClipboardHandler.kt
@@ -4,5 +4,5 @@ import android.content.Context
 
 interface ClipboardHandler {
     suspend fun copy(context: Context, copiedMessage: String)
-    suspend fun getLatestRecord(context: Context): String?
+    suspend fun getLatestRecord(context: Context): String
 }
