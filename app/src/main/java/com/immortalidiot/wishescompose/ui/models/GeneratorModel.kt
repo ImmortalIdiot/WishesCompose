cf4c31223be6e666f506e887b54bfc68c7536cac
ImmortalIdiot <max.krasulya@yandex.ru>
2024-10-09 02:49:25 +0300

    feat: add dayWish and nightWish to the model

--- app/src/main/java/com/immortalidiot/wishescompose/ui/models/GeneratorModel.kt
+++ app/src/main/java/com/immortalidiot/wishescompose/ui/models/GeneratorModel.kt
@@ -1,5 +1,7 @@
 package com.immortalidiot.wishescompose.ui.models
 
 data class GeneratorModel(
-    val emojis: String
+    val emojis: String,
+    val dayWish: String? = null,
+    val nightWish: String? = null
 )
