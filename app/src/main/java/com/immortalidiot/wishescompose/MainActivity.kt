348df36f21f1b451b1a36e6e15e1f1f04fba8feb
ImmortalIdiot <max.krasulya@yandex.ru>
2024-10-09 05:02:14 +0300

    feat: create snackbar local provider

--- app/src/main/java/com/immortalidiot/wishescompose/MainActivity.kt
+++ app/src/main/java/com/immortalidiot/wishescompose/MainActivity.kt
@@ -8,11 +8,15 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.navigation.compose.rememberNavController
 import com.immortalidiot.wishescompose.navigation.MainScreenFlow
+import com.immortalidiot.wishescompose.providers.LocalSnackbarHostState
 import com.immortalidiot.wishescompose.ui.theme.BackgroundEnd
 import com.immortalidiot.wishescompose.ui.theme.BackgroundStart
 import dagger.hilt.android.AndroidEntryPoint
@@ -27,22 +31,25 @@ class MainActivity : ComponentActivity() {
 
         super.onCreate(savedInstanceState)
         setContent {
+            val snackbarHostState = remember { SnackbarHostState() }
             val navHostController = rememberNavController()
 
-            Box(
-                modifier = Modifier
-                    .background(
-                        brush = Brush.linearGradient(
-                            colors = listOf(
-                                BackgroundStart,
-                                BackgroundEnd
+            CompositionLocalProvider(LocalSnackbarHostState provides snackbarHostState) {
+                Box(
+                    modifier = Modifier
+                        .background(
+                            brush = Brush.linearGradient(
+                                colors = listOf(
+                                    BackgroundStart,
+                                    BackgroundEnd
+                                )
                             )
                         )
-                    )
-                    .fillMaxSize(),
-                contentAlignment = Alignment.Center
-            ) {
-                MainScreenFlow(navHostController = navHostController)
+                        .fillMaxSize(),
+                    contentAlignment = Alignment.Center
+                ) {
+                    MainScreenFlow(navHostController = navHostController)
+                }
             }
         }
     }
